d39f5bf4e0be09e0de1943ea0eb91d86ea7af0df
root <Collinsanyona66@gmail.comgit config --global user.email Collinsanyona66@gmail.comcd>
2024-02-25 11:39:22 -0800

    1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,40 +1,64 @@
 #!/usr/bin/python3
-"""
-Using what you did in the task #0,
-extend your Python script to export
-data in the CSV format.
 
-Requirements:
 
-Records all tasks that are owned by this employee
-Format must be:
-"USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
-File name must be: USER_ID.csv
 """
+Retrieve and display the todo list progress for a specific employee.
+
+Args:
+employee_id (int): The ID of the employee for whom to fetch progress.
+
+Returns:
+None
+"""
+
+import requests
+import sys
+
 if __name__ == "__main__":
-    import csv
-    import json
-    import requests
-    import sys
-    # using this url https://jsonplaceholder.typicode.com/todos/
-    # add a query string of userId = 2 using the requests module
-    url1 = "https://jsonplaceholder.typicode.com/todos"
-    url2 = f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}"
-    payload = {"userId": sys.argv[1]}
-    # a sinle variable used to accept the response
-    # after request is made using the module
-    req_rep1 = requests.get(url1, params=payload)
-    req_rep2 = requests.get(url2)
-    req_rep1 = req_rep1.json()
-    req_rep2 = req_rep2.json()
-    # file name depends on id
-    filename = f"{sys.argv[1]}.csv"
-    with open(filename, 'w', newline='') as csvfile:
-        # create a csv writer object
-        data_writer = csv.writer(csvfile, delimiter=",", quotechar='"',
-                                 quoting=csv.QUOTE_ALL)
-        # iterate through the first request only and use the value of some key
-        # and use the username of the second request for every iteration
-        for data in req_rep1:
-            data_writer.writerow([data["userId"], req_rep2["username"],
-                                 data["completed"], data["title"]])
+
+    """
+    Gets the employee ID
+
+    Puts the links of APIs into variables
+
+    Retrieves data from the API and parses the data
+
+    Uses the data to generate a progress report for each employee
+    """
+
+    # Check for the correct number of arguments
+    if len(sys.argv) != 2:
+        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
+
+    # Get the employee ID from the command line argument
+    employee_id = sys.argv[1]
+
+    # Define the API endpoints for todos and users
+    todo_url = 'https://jsonplaceholder.typicode.com/todos'
+    user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+
+    # Send HTTP GET requests to retrieve data from the API
+    todo_response = requests.get(todo_url)
+    user_response = requests.get(user_url)
+
+    # Parse the JSON responses into Python dictionaries
+    todo_data = todo_response.json()
+    user_data = user_response.json()
+
+    if 'name' in user_data:
+        name = user_data['name']
+
+    # Filter completed and total tasks for the specified employee
+    completed_tasks = [task for task in todo_data
+                       if task['userId'] == int(employee_id) and
+                       task['completed']]
+    total_tasks = [task for task in todo_data
+                   if task['userId'] == int(employee_id)]
+
+    # Display the employee's todo list progress
+    fin = len(completed_tasks)
+    sum = len(total_tasks)
+    print(f"Employee {name} is done with tasks({fin}/{sum}):")
+    for task in completed_tasks:
+        print("\t " + task['title'])
